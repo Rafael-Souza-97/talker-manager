4f5807a367774db7e4aebf4a19c5c2e994510bb7
Rafael Souza <souza_rafael.97@outlook.com>
2022-11-04 16:21:01 -0300

    Corrige erros de Lint

--- src/index.js
+++ src/index.js
@@ -23,16 +23,14 @@ app.get('/talker', async (_req, res) => {
 app.get('/talker/:id', async (req, res) => {
   const users = await getTalkerUsers();
   const { id } = req.params;
-  const { name, age, talk } = req.body;
 
   const userId = users.find((user) => user.id === Number(id));
-  if (!userId) return res.status(404).json({
-    "message": "Pessoa palestrante não encontrada"
-  });
 
-  users.name = name;
-  users.age = age;
-  users.talk = talk;
+  if (!userId) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
 
   return res.status(200).json(userId);
 });
