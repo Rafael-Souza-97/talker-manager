878f6f5514d40502db9467b74e79165280eabc78
Rafael Souza <souza_rafael.97@outlook.com>
2022-11-04 15:42:41 -0300

    Adiciona Requisito 1: Realizado configurações iniciais e requisição.

---
+++ src/handleTalkerUsers.js
@@ -0,0 +1,13 @@
+const { readFile } = require('fs').promises; 
+const path = require('path');
+
+const usersDb = path.resolve(__dirname, '.', 'talker.json');
+
+const getTalkerUsers = async () => {
+  const response = await readFile(usersDb, 'utf8');
+  return JSON.parse(response);
+}
+
+module.exports = {
+  getTalkerUsers
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { getTalkerUsers } = require('./handleTalkerUsers');
 
 const app = express();
+app.use(express.json());
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -11,6 +13,12 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', async (_req, res) => {
+  const users = await getTalkerUsers();
+  if (users.length === 0) return res.status(200).json([]);
+  return res.status(200).json(users);
+})
 
 app.listen(PORT, () => {
   console.log('Online');
